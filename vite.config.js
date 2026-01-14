f33c180a73db868eb6484cf66a6cca76964056bd
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2026-01-13 23:13:08 -0500

    v1.99

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
-plugins: [react()],
+  plugins: [react()],
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Service-Worker-Allowed': '/'
+    }
+  }
 });
